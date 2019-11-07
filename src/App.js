771e74d6414e647c71e8111897dba6d4f2c20d11
Erick <moreno240190@gmail.com>
2019-11-07 13:49:13 -0600

    Instalacion de Axios y actualizacion del State

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import React, { Component } from 'react';
 import Users from './components/users/Users';
+import axios from 'axios';
 
 export default class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      'https://api.github.com/users?since=135%3E;%20rel=%22next%22'
+    );
+
+    this.setState({ users: res.data, loading: false });
+  }
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} user={this.state.users}/>
         </div>
       </div>
     );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,39 +2,18 @@ import React, { Component } from 'react';
 import UsersItem from './UsersItem';
 
 export default class Users extends Component {
-  state = {
-    users: [
-      {
-        id: '1',
-        login: 'simonjefford',
-        avatar_url: 'https://avatars2.githubusercontent.com/u/136?v=4',
-        html_url: 'https://github.com/simonjefford'
-      },
-      {
-        id: '2',
-        login: 'josh',
-        avatar_url: 'https://avatars2.githubusercontent.com/u/137?v=4',
-        html_url: 'https://github.com/josh'
-      },
-      {
-        id: '3',
-        login: 'stevedekorte',
-        avatar_url: 'https://avatars3.githubusercontent.com/u/138?v=4',
-        html_url: 'https://github.com/stevedekorte'
-      }
-    ]
-  };
-  // me quede en el video 3 minuto 02:51
+
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map(user => (
+        {this.props.user.map(user => (
           <UsersItem key={user.id} user={user} />
         ))}
       </div>
     );
   }
 }
+
 const userStyle = {
   display : 'grid',
   gridTemplateColumns: 'repeat(3, 1fr)',
