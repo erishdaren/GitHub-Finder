55113275b54ee96cb89727d07f230b4dbaca22f3
Erick <moreno240190@gmail.com>
2019-11-08 14:27:11 -0600

    Refactorizando Componentes

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,21 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UsersItem from './UsersItem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
 
-export default class Users extends Component {
-
-  render() {
+const Users = ({ user, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
-        {this.props.user.map(user => (
+        {user.map(user => (
           <UsersItem key={user.id} user={user} />
         ))}
       </div>
     );
   }
+};
+
+UsersItem.propTypes = {
+user:PropTypes.array.isRequired,
+loading: PropTypes.bool.isRequired
 }
 
+export default Users;
 const userStyle = {
-  display : 'grid',
+  display: 'grid',
   gridTemplateColumns: 'repeat(3, 1fr)',
-  gridGap : '1rem'
-}
+  gridGap: '1rem'
+};
