3b380c2e8bdc464530d92cd0d70409bb6b1d0a49
Erick <moreno240190@gmail.com>
2019-11-19 20:36:02 -0600

    Galeria de Usuarios terminado

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default class App extends Component {
       `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
                               &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-
+// me quede en el video 7 minito 03.51
     this.setState({ users: res.data, loading: false });
   }
   render() {
